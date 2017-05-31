da31736c2a543bde9431a1d3d881376365aa2f3d
tk1ej <jose.beuchat@six-group.com>
2017-05-31 13:17:40 +0200

    Add web service

---
+++ src/main/java/org/openshift/JaxrsConfig.java
@@ -0,0 +1,12 @@
+package org.openshift;
+
+/**
+ * Created by tk1ej on 31.05.2017.
+ */
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class JaxrsConfig extends Application {
+}
